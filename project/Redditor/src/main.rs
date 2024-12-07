ed612310887f49cffd89c15adce23d2a93b3725e
Rusu-Razvan <georgerusu984@gmail.com>
2024-12-07 19:37:45 +0200

    parsing json and showing the relevant information

--- project/Redditor/src/main.rs
+++ project/Redditor/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), ureq::Error> {
+    let url = "https://www.reddit.com/r/pics/new.json";
+    let response = ureq::get(&url)
+        .set("Header", "header value")
+        .call();
+        
+        if response.is_ok() 
+        {
+            let json:serde_json::Value= response.unwrap().into_json()?;
+        
+
+        for post in json["data"]["children"].as_array().unwrap() {
+            println!("Title: {}", post["data"]["title"].as_str().unwrap_or("[No title]"));
+            println!("Author: {}", post["data"]["author"]);
+            println!("Creation date: {}", post["data"]["created_utc"]);
+            println!("URL: https://www.reddit.com{}", post["data"]["permalink"].as_str().unwrap_or("#"));
+
+            println!("\n\n");
+        }
+
+    }
+       
+
+
+    Ok(())
 }
