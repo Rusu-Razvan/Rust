35a294234cd1f9b0f6ec872f807d40edf382dda1
Rusu-Razvan <georgerusu984@gmail.com>
2024-12-14 14:14:26 +0200

    Can now take arguments from the command line

--- project/Redditor/src/main.rs
+++ project/Redditor/src/main.rs
@@ -1,5 +1,30 @@
+use std::io;
 fn main() -> Result<(), ureq::Error> {
-    let url = "https://www.reddit.com/r/pics/new.json";
+
+    let mut input = String::new();
+    let mut subreddit:&str = "foxes"; //default subreddit
+    let mut sort_order:&str = "hot"; //default sort order
+
+    println!("Usage:[subreddit] [sort order(hot/new/top)] (Default subreddit: foxes, Default sort order: hot)");
+    println!("Enter the name of the subreddit you want to see: ");
+
+
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+           
+           input = input.trim().to_string();
+           
+            let mut parts = input.split_whitespace();
+             subreddit = parts.next().unwrap_or("foxes");
+             sort_order = parts.next().unwrap_or("hot");
+        }
+        Err(error) => println!("error: {error}"),
+    }
+
+
+    let url = format!("https://www.reddit.com/r/{}/{}.json", subreddit, sort_order);
+
+   
     let response = ureq::get(&url)
         .set("Header", "header value")
         .call();
@@ -7,6 +32,7 @@ fn main() -> Result<(), ureq::Error> {
         if response.is_ok() 
         {
             let json:serde_json::Value= response.unwrap().into_json()?;
+        
         
 
         for post in json["data"]["children"].as_array().unwrap() {
@@ -19,8 +45,10 @@ fn main() -> Result<(), ureq::Error> {
         }
 
     }
-       
-
+    else
+    {
+        println!("Error: {}", response.unwrap_err());
+    }
 
     Ok(())
 }
